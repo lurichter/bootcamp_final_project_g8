5d3b04210319e5326dcb31e1cae648b7ddfdcdef
gab-duarte <gabriel.duarte@mercadolivre.com>
2021-07-06 19:46:52 -0300

    change DTO files

--- src/main/java/com/mercadolibre/group8_bootcamp_finalproject/dtos/ProductDTO.java
+++ src/main/java/com/mercadolibre/group8_bootcamp_finalproject/dtos/ProductDTO.java
@@ -3,8 +3,6 @@ package com.mercadolibre.group8_bootcamp_finalproject.dtos;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @Builder
 public class ProductDTO {
@@ -13,7 +11,5 @@ public class ProductDTO {
     private String description;
     private Double minTemperature;
     private Double maxTemperature;
-    private Long batchNumber;
-    private LocalDateTime dueDate;
     private Double price;
 }
