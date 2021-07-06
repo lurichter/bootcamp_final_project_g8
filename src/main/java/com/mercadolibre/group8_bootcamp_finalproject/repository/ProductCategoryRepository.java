b71b18d9367e88bef49908d29aad776bce382ebf
gab-duarte <gabriel.duarte@mercadolivre.com>
2021-07-06 19:46:09 -0300

    create queries

--- src/main/java/com/mercadolibre/group8_bootcamp_finalproject/repository/ProductCategoryRepository.java
+++ src/main/java/com/mercadolibre/group8_bootcamp_finalproject/repository/ProductCategoryRepository.java
@@ -2,6 +2,11 @@ package com.mercadolibre.group8_bootcamp_finalproject.repository;
 
 import com.mercadolibre.group8_bootcamp_finalproject.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+    @Query(value = "SELECT p from ProductCategory p WHERE p.name = :categoryName")
+    ProductCategory findByCategoryName(@Param("categoryName") String categoryName);
 }
--- src/main/java/com/mercadolibre/group8_bootcamp_finalproject/repository/ProductRepository.java
+++ src/main/java/com/mercadolibre/group8_bootcamp_finalproject/repository/ProductRepository.java
@@ -2,10 +2,13 @@ package com.mercadolibre.group8_bootcamp_finalproject.repository;
 
 import com.mercadolibre.group8_bootcamp_finalproject.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findAllByProductCategory(String category);
+    @Query(value = "SELECT p FROM Product p WHERE p.productCategory.id = :categoryId")
+    List<Product> findAllByProductCategory(@Param("categoryId")Long categoryId);
 }
