feabd4210c41d9609282592a8f4dd9540edf5ebc
gab-duarte <gabriel.duarte@mercadolivre.com>
2021-07-11 20:11:44 -0300

    refactoring dto

--- src/main/java/com/mercadolibre/group8_bootcamp_finalproject/service/IProductService.java
+++ src/main/java/com/mercadolibre/group8_bootcamp_finalproject/service/IProductService.java
@@ -1,13 +1,14 @@
 package com.mercadolibre.group8_bootcamp_finalproject.service;
 
 import com.mercadolibre.group8_bootcamp_finalproject.dtos.ProductDTO;
+import com.mercadolibre.group8_bootcamp_finalproject.dtos.response.ProductListDTO;
 import com.mercadolibre.group8_bootcamp_finalproject.model.enums.ProductCategoryEnum;
 
 import java.util.Set;
 
 public interface IProductService {
 
-    Set<ProductDTO> getAllProducts();
+    ProductListDTO getAllProducts();
 
-    Set<ProductDTO> getAllProductsByCategory(ProductCategoryEnum category);
+    ProductListDTO getAllProductsByCategory(ProductCategoryEnum category);
 }
