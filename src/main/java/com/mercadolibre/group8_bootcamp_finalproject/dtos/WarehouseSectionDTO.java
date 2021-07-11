5ff6bab38c6cc0b170f31b7a1777a5dfbb225d7c
gab-duarte <gabriel.duarte@mercadolivre.com>
2021-07-11 20:09:12 -0300

    Add builder

--- src/main/java/com/mercadolibre/group8_bootcamp_finalproject/dtos/WarehouseSectionDTO.java
+++ src/main/java/com/mercadolibre/group8_bootcamp_finalproject/dtos/WarehouseSectionDTO.java
@@ -1,10 +1,12 @@
 package com.mercadolibre.group8_bootcamp_finalproject.dtos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
 public class WarehouseSectionDTO {
 
     @NotNull(message = "section code is required")
